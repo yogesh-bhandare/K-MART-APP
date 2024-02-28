7305eba6c5bf09984e1e88c358a9f2285ac0f800
gityb8 <statichub8@gmail.com>
2024-02-29 02:03:31 +0530

    Weather Feature Upgrade

--- client/AdditionalScreens/WeatherScreen.js
+++ client/AdditionalScreens/WeatherScreen.js
@@ -1,19 +1,11 @@
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { useNavigation } from '@react-navigation/native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const WeatherScreen = () => {
-  const navigation = useNavigation();
-
-  const [showSearch, toggelSearch] = useState(false);
-  const [locations, setLocations] = useState([1,2,3]);
-
   return (
-    <SafeAreaView className="flex flex-1">
-      
-    </SafeAreaView>
+    <View>
+      <Text>WeatherScreen</Text>
+    </View>
   )
 }
 
