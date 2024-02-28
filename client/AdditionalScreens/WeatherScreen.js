8d983d6258eabf8fcb8384a6c2b7a0312539ce2a
gityb8 <statichub8@gmail.com>
2024-02-28 13:48:36 +0530

    Added weather screen heading

--- client/AdditionalScreens/WeatherScreen.js
+++ client/AdditionalScreens/WeatherScreen.js
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const WeatherScreen = () => {
   return (
-    <View>
-      <Text>WeatherScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View className="h-14 px-4 py-2 flex flex-row items-center border-gray-200 border-b-2">
+        <TouchableOpacity onPress={() => navigation.navigate("MoreScreen")}>
+          <Ionicons name="chevron-back" size={22} color={"rgb(75 85 99)"} />
+        </TouchableOpacity>
+        <Text className=" text-sm text-gray-600">Wheather</Text>
+      </View>
+      
+    </SafeAreaView>
   )
 }
 
